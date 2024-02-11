8febfa2bb0122053f2244fec90f1b7dcbdb2e5cb
POPYL99 <54652668+POPYL99@users.noreply.github.com>
2024-02-11 09:31:06 +0200

    Update fbconfig.js

---
+++ fbconfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyCRZ4K_qswEr6M2Pz5x3IwIL12n3C9EJr0",
+  authDomain: "gadgetapp-58074.firebaseapp.com",
+  projectId: "gadgetapp-58074",
+  storageBucket: "gadgetapp-58074.appspot.com",
+  messagingSenderId: "1052628431236",
+  appId: "1:1052628431236:web:448fd9bb1ba972038eadd1"
+};
+
+const app = initializeApp(firebaseConfig);
+export const database =getFirestore(app)
